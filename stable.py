461a0d5b4132e387f6e201e2c7a3e3b7896993a4
MARTY LUCA FEDERICO <46960905@ort>
2023-06-06 15:58:02 -0300

    Function get engines

--- stable.py
+++ stable.py
@@ -1,41 +1,52 @@
-import base64
 import os
 import requests
+import json
 
-engine_id = "stable-diffusion-v1-5"
+from dotenv import load_dotenv, find_dotenv 
+
+# * Find the path of the dotenv file
+
+dotenv_path = find_dotenv()
+
+# * Load the enviromental variables from the dotenv path
+
+load_dotenv(dotenv_path)
+
+# * Endpoint for getting all the current Stable Diffusion Engines
+
 api_host = os.getenv('API_HOST', 'https://api.stability.ai')
-api_key = os.getenv("STABILITY_API_KEY")
 
+endpoint_engines = f"{api_host}/v1/engines/list"
+
+# * Getting the API key and checking if it actually exists
+
+api_key = os.getenv("STABILITY_API_KEY")
 if api_key is None:
     raise Exception("Missing Stability API key.")
 
-response = requests.post(
-    f"{api_host}/v1/generation/{engine_id}/text-to-image",
-    headers={
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": f"Bearer {api_key}"
-    },
-    json={
-        "text_prompts": [
-            {
-                "text": "A lighthouse on a cliff"
-            }
-        ],
-        "cfg_scale": 7,
-        "clip_guidance_preset": "FAST_BLUE",
-        "height": 512,
-        "width": 512,
-        "samples": 1,
-        "steps": 30,
-    },
-)
+def get_engines(endpoint, api_key):
 
-if response.status_code != 200:
-    raise Exception("Non-200 response: " + str(response.text))
+# * Do the request with the following parameters:
+# * METHOD: Get
+# * ENDPOINT: /v1/engines/list
+# * HEADER: Stable Diffusion API Key
 
-data = response.json()
+    response = requests.get(endpoint, headers={"Authorization": f"Bearer {api_key}"})
+    
+    # * In case the status code is not 200 (that means everything went correct), raise an error
 
-for i, image in enumerate(data["artifacts"]):
-    with open(f"./out/v1_txt2img_{i}.png", "wb") as f:
-        f.write(base64.b64decode(image["base64"]))
+    if response.status_code != 200:
+        raise Exception("Non-200 response: " + str(response.text))
+
+    # * Receive the engines JSON
+    engines = response.json()
+
+    # * Create a new JSON file and write the engines to it
+    # * Then, close the file
+
+    jsonString = json.dumps(engines)
+    enginesJSON = open("engines.json", "w")
+    enginesJSON.write(jsonString)
+    enginesJSON.close()
+
+    return engines
