6f7f03cc0a5ac1769f72be91d325ea1aa2ac8bd4
LucaFedericoMarty <lucamarty.ar@gmail.com>
2023-06-15 10:07:46 -0300

    Advance prompt function

--- stable_fk.py
+++ stable_fk.py
@@ -6,14 +6,17 @@ import base64
 from diffusers import DiffusionPipeline
 import torch
 
-def create_prompt(price, style, enviroment):
-    enviroment_w = weight_keyword(enviroment, 0.9)
-    style_w = weight_keyword(style, 0.5)
-    resolution = "8k"
-    picture_style = "hyperrealistic"
-    prompt = f"Interior design of a {enviroment_w}, {style}"
-    print(prompt)
-    return []
+def create_prompt(budget, style, enviroment, region_weather):
+  budget += " budget"
+  budget_w = weight_keyword(budget, 0.6)
+  enviroment_w = weight_keyword(enviroment, 0.9)
+  style_w = weight_keyword(style, 0.5)
+  region_weather += " weather"
+  region_weather_w = weight_keyword(region_weather, 0.3)
+  resolution = "8k"
+  picture_style = "hyperrealistic"
+  prompt = f"Interior design of a {enviroment_w}, {style_w}, for a {region_weather_w}, {picture_style}, {budget_w}, in {resolution}"
+  return prompt
 
 def weight_keyword(keyword, weight):
     weighted_keyword = {keyword : weight}
