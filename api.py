e2285fc73c22868658f5e046e96d3d90dcf34ccd
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2023-06-23 17:06:44 -0300

    Prompt and weight functions

--- api.py
+++ api.py
@@ -16,6 +16,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+def weight_keyword(keyword : str, weight : float) -> dict:
+    weighted_keyword = {keyword : weight}
+    for key, value in weighted_keyword.items():
+        string_weighted_keyword = (f'{key} : {value}')
+    return string_weighted_keyword
+
+def create_prompt(budget : str, style : str , environment : str, region_weather : str) -> str:
+  budget += " budget"
+  budget_w = weight_keyword(budget, 0.5)
+  environment_w = weight_keyword(environment, 1)
+  style_w = weight_keyword(style, 0.6)
+  region_weather += " weather"
+  region_weather_w = weight_keyword(region_weather, 0.2)
+  resolution = "8k"
+  picture_style = "hyperrealistic"
+  prompt = f"Interior design of a {environment_w}, {style_w}, for a {region_weather_w}, {picture_style}, {budget_w}, 8k uhd, dslr, soft lighting, high quality, film grain, Fujifilm XT3"
+  return prompt
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
