9a64d9801e414c101961a312b1ca5fd3f930520b
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2023-06-23 20:33:49 -0300

    feat(function): Image grid for various images

--- api.py
+++ api.py
@@ -34,6 +34,20 @@ def create_prompt(budget : str, style : str , environment : str, region_weather 
   prompt = f"Interior design of a {environment_w}, {style_w}, for a {region_weather_w}, {picture_style}, {budget_w}, 8k uhd, dslr, soft lighting, high quality, film grain, Fujifilm XT3"
   return prompt
 
+def image_grid(imgs, rows=2, cols=2):
+  # * Get the width and height of the first image
+  width, height = imgs[0].size
+  # * Create the image grid, with the size of the images
+    # * Example: 4 images of 512x512
+      # * Height: 512 X 2 = 1024
+      # * Width: 512 X 2 = 1024
+  grid = Image.new("RGB", size=(cols * width, rows * height))
+
+  # * Paste images into the grid
+  for i, img in enumerate(imgs):
+      grid.paste(img, box=(i % cols * width, i // cols * height))
+  return grid
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
