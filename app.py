91aa94971aa16244260e2c59105e09b2c8dd6f3e
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2023-06-25 11:26:57 -0300

    BREAKING CHANGE(app): Streamlit app deployment

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from diffusers import ( DiffusionPipeline,
+StableDiffusionImg2ImgPipeline,
+StableDiffusionInpaintPipeline,
+EulerAncestralDiscreteScheduler)
+import torch
+from PIL import Image
+
+from helper_functions import create_prompt, image_grid, choose_scheduler, load_pipeline, models
+
+txt2img_model, img2img_model, inpaint_model = load_pipeline(model_id = "SG161222/Realistic_Vision_V1.4", revision="fp16", torch_dtype=torch.float16, scheduler=EulerAncestralDiscreteScheduler)
+
+def txt2img(prompt : str , steps : int, cfg : float, num_images : int):
+    images = txt2img_model(prompt, num_inference_steps=steps, guidance_scale=cfg, num_images_per_prompt=num_images).images
+    grid = image_grid(images)
+    return images
+
+st.title("DesAIgner App Deployment for testing :house_with_garden:")
+
+st.markdown("## Inputs for creating an image ##")
+
+budget = st.selectbox("Budget", ('Low', 'Medium', 'High'))
+
+style = st.selectbox("Style", ('Minimalist', 'Modern', 'Eclectic', 'Traditional', 'Bohemian', 'Shabby Chic', 'Coastal', 'Rustic', 'Industrial', 'Scandinavian', 'Mediterranean', 'Art Deco', 'Transitional', 'Farmhouse', 'Oriental'))
+
+environment = st.selectbox("Environment", ('Kitchen', 'Living Room', 'Bedroom', 'Bathroom'))
+
+region_weather = st.selectbox("Region", ('Cold', 'Hot', 'Snowy', 'Rainy', 'Tropical'))
+
+num_images = st.slider('How many photos do you want?', 0, 10, 4)
+
+prompt = create_prompt(budget=budget, style=style, environment=environment, region_weather=region_weather)
+
+if st.button("Create image :sparkles:"):
+    images = txt2img(prompt=prompt, steps=50, cfg=4.5, num_images=num_images)
+    st.images(images=images)  
+
+
+
