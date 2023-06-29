8ee8129eaf05ef1e3accef2382722ca107a3a02b
LucaFedericoMarty <lucamarty.ar@gmail.com>
2023-06-29 12:12:26 -0300

    feat(api): Changed body of the request

--- api.py
+++ api.py
@@ -14,31 +14,34 @@ import numpy as np
 from io import BytesIO
 from accelerate import PartialState
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Request
 
 from helper_functions import weight_keyword, create_prompt, image_grid, choose_scheduler, load_pipelines, models  
 
 app = FastAPI()
 
-txt2img_model, img2img_model, inpaint_model = load_pipelines(model_id = "SG161222/Realistic_Vision_V1.4", revision="fp16", torch_dtype=torch.float16, scheduler=EulerAncestralDiscreteScheduler)
+txt2img_model, img2img_model, inpaint_model, image_variation_model = load_pipelines(model_id = "SG161222/Realistic_Vision_V1.4", revision="fp16", torch_dtype=torch.float16, scheduler=EulerAncestralDiscreteScheduler)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/txt2img", methods=["POST"])
-def txt2img(prompt : str , path : str, steps : int, cfg : float, num_images : int):
+async def txt2img(request : Request):
     imagesstr = []
+    data = await request.json()
+    prompt = data["prompt"]
+    steps = data["steps"]
+    cfg = data["guidance scale"]
+    num_images = data["number images"]
     images = txt2img_model(prompt, num_inference_steps=steps, guidance_scale=cfg, num_images_per_prompt=num_images).images
     for num_image in range(len(images)):
         image = images[num_image]
-        image.save(f"{path}/Test Image {num_image}.png")
         buffer = BytesIO()
         image.save(buffer, format="PNG")
         imgstr = base64.b64encode(buffer.getvalue())
         imagesstr.append(imgstr)
     grid = image_grid(images)
-    grid.save(f"{path}/Image Grid.png")
     return Response(content=imagesstr, media_type="image/png")
 
 @app.route("/inpaint", methods=["POST"])
