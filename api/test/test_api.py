b0a727445778c2da8aba408055ced42cdd344e6e
LucaFedericoMarty <lucamarty.ar@gmail.com>
2023-08-16 15:58:26 -0300

    feat(test): Started txt2img test functions

--- api/test/test_api.py
+++ api/test/test_api.py
@@ -2,3 +2,6 @@ from fastapi.testclient import TestClient
 from api.api import app
 
 client = TestClient(app=app)
+
+def test_text_to_image():
+    response = client.post('/txt2img/v2/v1', json={"budget": "Low", "style": "Oceanic","environment": "Bedroom","weather": "Tropical","disability": "x","steps": 20,"guidance_scale": 7,"num_images": 2})
