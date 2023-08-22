1eea6e5271b44ed5f57c7d739055487371575ef7
LucaFedericoMarty <lucamarty.ar@gmail.com>
2023-08-22 15:55:12 -0300

    fix: Tried to connect to front-end

--- api/api.py
+++ api/api.py
@@ -86,7 +86,7 @@ async def add_process_time_header(request: Request, call_next):
     
 # * Origings for CORS requests    
     
-origins = ["http://localhost:8000", "http://localhost:3000"]
+origins = ["http://localhost:8000", "http://localhost:3000", "http://localhost:3000/create"]
 
 # * Adding CORS Class to the Middleware
 
@@ -112,7 +112,7 @@ app.add_middleware(
 
 """
 
-txt2img_model, img2img_model, inpaint_model = load_all_pipelines(model_id = "SG161222/Realistic_Vision_V5.0_noVAE", inpaint_model_id = "../Models/Realistic_Vision_V5.1_fp16-no-ema-inpainting.safetensors")
+txt2img_model, img2img_model, inpaint_model = load_all_pipelines(model_id = "SG161222/Realistic_Vision_V5.0_noVAE", inpaint_model_id = r"../Models/Realistic_Vision_V5.1_fp16-no-ema-inpainting.safetensors")
 mlsd_detector =  load_mlsd_detector(model_id='lllyasviel/ControlNet')#revision="fp16", #torch_dtype=torch.float16)
 
 # * Create the negative prompt for avoiding certain concepts in the photo
@@ -200,7 +200,9 @@ def txt2imgjson(budget : Annotated[str , Query(title="Budget of the re-design", 
     return JSONResponse(content=jsonCompatibleImages, headers=headers)
 
 @app.post("/txt2img/v2/v1", response_model=ImageResponse, tags=["text2image"])
-def txt2imgclass(params: Txt2ImgParamas, api_key: APIKey = Security(get_api_key)):
+def txt2imgclass(params: Txt2ImgParamas, 
+                 #api_key: APIKey = Security(get_api_key)
+                 ):
     
     """Text-to-image route request that performs a text-to-image process using a pre-trained Stable Diffusion Model"""
 
@@ -319,7 +321,8 @@ def img2img(budget : Annotated[str , Query(title="Budget of the re-design", desc
 @app.post("/img2img/v2", response_model= ImageResponse, tags=["image2image"])
 def img2img(params: Img2ImgParams = Depends(),
             input_image : UploadFile = File(title="Image desired to re-design", description="The model will base the re-design based on the characteristics of this image"),
-            api_key: str = Security(get_api_key),):
+            #api_key: str = Security(get_api_key),
+            ):
     
     """Image-to-image route request that performs a image-to-image process using a pre-trained Stable Diffusion Model"""
 
