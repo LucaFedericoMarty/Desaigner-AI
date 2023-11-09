85acf223cd2e3619a5a2c9079822032c78e8b741
LucaFedericoMarty <lucamarty.ar@gmail.com>
2023-11-09 10:41:04 -0300

    Started tests

--- api/test/test_api.py
+++ api/test/test_api.py
@@ -1,7 +1,8 @@
 from fastapi.testclient import TestClient
 from starlette.status import HTTP_200_OK, HTTP_413_REQUEST_ENTITY_TOO_LARGE, HTTP_415_UNSUPPORTED_MEDIA_TYPE
 from api.api import app
-from schemas import IMAGES_B64
+from schemas import IMAGES_B64, IMAGE
+import pytest
 
 client = TestClient(app=app)
 
@@ -14,6 +15,15 @@ request_data = {
     "steps": 20,
     "guidance_scale": 7,
     "num_images": 2}
+
+@pytest.fixture
+def return_image(image : IMAGE):
+    return image
+
+@pytest.mark.parametrize()
+
+def test_base64_encoding_512_images():
+    """Test for images of resolution base64 encoding"""
 
 def test_text_to_image():
     """Test for text to image endpoint
